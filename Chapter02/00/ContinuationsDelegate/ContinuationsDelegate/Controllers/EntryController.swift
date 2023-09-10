4c7c86a1ca2570b06ad26f01880a5f5195cb090b
Takeshi Suzuki <tockrock@gmail.com>
2023-09-10 19:08:45 +0900

    Making EntryController our delegate

--- Chapter02/00/ContinuationsDelegate/ContinuationsDelegate/Controllers/EntryController.swift
+++ Chapter02/00/ContinuationsDelegate/ContinuationsDelegate/Controllers/EntryController.swift
@@ -1,4 +1,5 @@
 import Foundation
+import DelegateSupport
 
 @MainActor
 class EntryController: ObservableObject {
@@ -6,9 +7,20 @@ class EntryController: ObservableObject {
   @Published private(set) var isUpdating = false
   @Published private(set) var delta = "..."
   let suffix = ".circle"
+  lazy private var numberVendor = DelegatingVendor(delegate: self)
 }
 
 extension EntryController {
   func next() {
+  }
+}
+
+extension EntryController: VendorDelegate {
+  func vendorWillSelect(_ vendor: DelegatingVendor) {
+    <#code#>
+  }
+  
+  func vendor(_ vendor: DelegatingVendor, didSelect number: Int) {
+    <#code#>
   }
 }
