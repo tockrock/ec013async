90f0e6cf51d9ec8f88372bb3c764b24c293b485c
Takeshi Suzuki <tockrock@gmail.com>
2023-09-10 19:30:54 +0900

    Connect DelegateWrapper to DelegatingVendor

--- Chapter02/00/ContinuationsDelegate/ContinuationsDelegate/Controllers/DelegateWrapper.swift
+++ Chapter02/00/ContinuationsDelegate/ContinuationsDelegate/Controllers/DelegateWrapper.swift
@@ -1,3 +1,17 @@
+import DelegateSupport
+
 class DelegateWrapper {
+  lazy private var numberVendor = DelegatingVendor(delegate: self)
+}
+
+extension DelegateWrapper: VendorDelegate {
+  func randomEntryNumber() async -> Int {
+    0
+  }
   
+  func vendorWillSelect(_ vendor: DelegatingVendor) {
+  }
+  
+  func vendor(_ vendor: DelegatingVendor, didSelect number: Int) {
+  }
 }
