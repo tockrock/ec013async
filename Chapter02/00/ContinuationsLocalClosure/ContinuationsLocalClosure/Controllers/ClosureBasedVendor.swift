14aad92bd7fc6eb6c5210da85c8118fbca4c968b
Takeshi Suzuki <tockrock@gmail.com>
2023-09-12 21:38:41 +0900

    Implementing the async version

--- Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/ClosureBasedVendor.swift
+++ Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/ClosureBasedVendor.swift
@@ -15,6 +15,10 @@ extension ClosureBasedVendor {
   }
   
   func selectRandomNumber() async -> (Int, Bool) {
-    
+    let numberBeforeChange = number
+    try? await Task.sleep(for: .seconds(0.5))
+    number = Int.random(in: 1...50)
+    let isGreater = number > numberBeforeChange
+    return (number, isGreater)
   }
 }
