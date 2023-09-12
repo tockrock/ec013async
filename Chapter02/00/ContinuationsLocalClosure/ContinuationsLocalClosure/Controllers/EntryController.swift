3f5a11513681023dc062b7cf3379e50c8eaa23ca
Takeshi Suzuki <tockrock@gmail.com>
2023-09-12 20:33:03 +0900

    EntryController

--- Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/EntryController.swift
+++ Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/EntryController.swift
@@ -6,10 +6,17 @@ class EntryController: ObservableObject {
   @Published private(set) var isUpdating = false
   @Published private(set) var delta = "..."
   let suffix = ".circle"
+  private let vendor = ClosureBasedVendor()
 }
 
 extension EntryController {
   func next() {
+    isUpdating = true
+    vendor.selectRandomNumber { number, isGreater in
+      self.entry = Entry(imageName: number.description + self.suffix)
+      self.delta = isGreater ? "+" : "-"
+      self.isUpdating = false
+    }
   }
 }
 
