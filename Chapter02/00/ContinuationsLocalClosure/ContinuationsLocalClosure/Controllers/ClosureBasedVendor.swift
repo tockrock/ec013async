2e75cc9c3d281332375ea66db1ce0708b5c8b4af
Takeshi Suzuki <tockrock@gmail.com>
2023-09-12 20:31:40 +0900

    Accepting and using the closure

--- Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/ClosureBasedVendor.swift
+++ Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/ClosureBasedVendor.swift
@@ -1,3 +1,15 @@
 class ClosureBasedVendor {
   private var number = 0
 }
+
+extension ClosureBasedVendor {
+    func selectRandomNumber(with completion: @escaping (Int, Bool) -> Void) {
+        Task {
+            let numberBeforeChange = number
+            try? await Task.sleep(for: .seconds(0.5))
+            number = Int.random(in: 1...50)
+            let isGreater = number > numberBeforeChange
+            completion(number, isGreater)
+        }
+    }
+}
