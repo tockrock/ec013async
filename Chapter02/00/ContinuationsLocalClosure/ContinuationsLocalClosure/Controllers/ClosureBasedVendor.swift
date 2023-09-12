7e90b9ed9623f85aef26360fb9126ef2d7566720
Takeshi Suzuki <tockrock@gmail.com>
2023-09-12 21:40:38 +0900

    Calling the new from the old

--- Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/ClosureBasedVendor.swift
+++ Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/ClosureBasedVendor.swift
@@ -6,10 +6,7 @@ extension ClosureBasedVendor {
   @MainActor
   func selectRandomNumber(with completion: @escaping (Int, Bool) -> Void) {
     Task {
-      let numberBeforeChange = number
-      try? await Task.sleep(for: .seconds(0.5))
-      number = Int.random(in: 1...50)
-      let isGreater = number > numberBeforeChange
+      let (number, isGreater) = await selectRandomNumber()
       completion(number, isGreater)
     }
   }
