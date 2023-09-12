924e1652a7d279ed3a10c535dded2b0906f0f35a
Takeshi Suzuki <tockrock@gmail.com>
2023-09-12 21:48:28 +0900

    Clean up

--- Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/ClosureBasedVendor.swift
+++ Chapter02/00/ContinuationsLocalClosure/ContinuationsLocalClosure/Controllers/ClosureBasedVendor.swift
@@ -3,15 +3,6 @@ class ClosureBasedVendor {
 }
 
 extension ClosureBasedVendor {
-  @available(*, deprecated, message: "Use async version of selectRandomNumber()")
-  @MainActor
-  func selectRandomNumber(with completion: @escaping (Int, Bool) -> Void) {
-    Task {
-      let (number, isGreater) = await selectRandomNumber()
-      completion(number, isGreater)
-    }
-  }
-  
   func selectRandomNumber() async -> (Int, Bool) {
     let numberBeforeChange = number
     try? await Task.sleep(for: .seconds(0.5))
