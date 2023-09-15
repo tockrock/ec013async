0817d5af07614a38bf1d83958ab289a28ba25ea8
Takeshi Suzuki <tockrock@gmail.com>
2023-09-15 23:52:42 +0900

    Async Alternative

--- Chapter02/08/ContinuationsClosure/ContinuationsClosure/Controllers/ToolDemo.swift
+++ Chapter02/08/ContinuationsClosure/ContinuationsClosure/Controllers/ToolDemo.swift
@@ -3,14 +3,22 @@ class ToolDemo{
 }
 
 extension ToolDemo {
+  @available(*, renamed: "demo1()")
   func demo1(completion: @escaping (Int, Bool) -> Void) {
     Task {
-      let numberBeforeChange = number
-      try? await Task.sleep(for: .seconds(0.5))
-      number = Int.random(in: 1...50)
-      let isGreater = number > numberBeforeChange
-      completion(number, isGreater)
+      let result = await demo1()
+      completion(result.0, result.1)
     }
+  }
+  
+  
+  func demo1() async -> (Int, Bool) {
+    let numberBeforeChange = number
+    try? await Task.sleep(for: .seconds(0.5))
+    number = Int.random(in: 1...50)
+    let isGreater = number > numberBeforeChange
+    return (number, isGreater)
+
   }
 }
 
