f3fb6ec7fce2b41d0af19af4ebbed068c06061ff
Takeshi Suzuki <tockrock@gmail.com>
2023-09-15 23:40:03 +0900

    Connect ClosureWrapper to VendorUsingClosure Connect ClosureWrapper to ClosureBasedVendor

--- Chapter02/00/ContinuationsClosure/ContinuationsClosure/Controllers/ClosureWrapper.swift
+++ Chapter02/00/ContinuationsClosure/ContinuationsClosure/Controllers/ClosureWrapper.swift
@@ -1,9 +1,16 @@
+import ClosureSupport
+
 class ClosureWrapper {
+  let vendor = ClosureBasedVendor()
   
 }
 
 extension ClosureWrapper {
   func randomNumber() async -> (Int, Bool) {
-    return (0, false)
+    return await withCheckedContinuation { continuation in
+      vendor.selectRandomNumber { number, isGreater in
+        continuation.resume(returning: (number, isGreater))
+      }
+    }
   }
 }
