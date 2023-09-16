9b7bed600ac2926a7800543d5100fb1b71fe7bdf
Takeshi Suzuki <tockrock@gmail.com>
2023-09-16 21:59:27 +0900

    One more thing

--- Chapter02/00/ContinuationsURLSession/ContinuationsURLSession/Controllers/VendorUsingURLSession.swift
+++ Chapter02/00/ContinuationsURLSession/ContinuationsURLSession/Controllers/VendorUsingURLSession.swift
@@ -4,39 +4,14 @@ class VendorUsingURLSession {
 }
 
 extension VendorUsingURLSession {
-  @MainActor
-  func selectRandomNumber(with completion:
-                          @escaping (Int?, Error?) -> Void) {
-    URLSession.shared
-      .dataTask(with: URLConstants.intURL) { data, _, error in
-        if let data,
-           let string = String(data: data, encoding: .utf8),
-           let number = Int(string) {
-          completion(number, nil)
-        } else {
-          completion(nil, error)
-        }
-      }
-      .resume()
-  }
   
   func randomNumber() async throws -> Int {
-    try await withCheckedThrowingContinuation { continuation in
-      URLSession.shared
-        .dataTask(with: URLConstants.intURL) { data, _, error in
-          if let data,
-             let string = String(data: data, encoding: .utf8),
-             let number = Int(string) {
-            continuation.resume(returning: number)
-          } else {
-            guard let error else {
-              continuation.resume(throwing: UnexpectedDataFromServerError())
-              return
-            }
-            continuation.resume(throwing: error)
-          }
-        }
-        .resume()
+    let (data, _) = try await URLSession.shared.data(from: URLConstants.intURL)
+    if let string = String(data: data, encoding: .utf8),
+       let number = Int(string) {
+      return number
+    } else {
+      throw UnexpectedDataFromServerError()
     }
   }
 }
