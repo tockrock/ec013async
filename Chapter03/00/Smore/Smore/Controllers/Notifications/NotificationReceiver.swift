4ce132a2328331abe56b6d0d1c3d608d2e3ce39e
Takeshi Suzuki <tockrock@gmail.com>
2023-09-18 19:31:01 +0900

    NotificationCenter.Notifications

--- Chapter03/00/Smore/Smore/Controllers/Notifications/NotificationReceiver.swift
+++ Chapter03/00/Smore/Smore/Controllers/Notifications/NotificationReceiver.swift
@@ -1,18 +1,19 @@
 import Foundation
 
 class NotificationReceiver {
+  let notifications = NotificationCenter.default
+    .notifications(named: NextNumberNotification.name)
+  
   var numbers: AsyncStream<Int> {
     AsyncStream(Int.self) { contiunation in
-      NotificationCenter
-        .default
-        .addObserver(forName: NextNumberNotification.name,
-                     object: nil, queue: nil) { notification in
+      Task {
+        for await notification in notifications {
           if let userInfo = notification.userInfo,
              let number = userInfo[NextNumberNotification.numberKey] as? Int {
             contiunation.yield(number)
           }
         }
-      
+      }
     }
   }
   
