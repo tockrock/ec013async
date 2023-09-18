220f0ebf6acf0d0a35c07d55bcafc809b73521ad
Takeshi Suzuki <tockrock@gmail.com>
2023-09-18 19:24:48 +0900

    Meet AsyncStream

--- Chapter03/00/Smore/Smore/Controllers/Notifications/NotificationReceiver.swift
+++ Chapter03/00/Smore/Smore/Controllers/Notifications/NotificationReceiver.swift
@@ -1,6 +1,21 @@
 import Foundation
 
 class NotificationReceiver {
+  var numbers: AsyncStream<Int> {
+    AsyncStream(Int.self) { contiunation in
+      NotificationCenter
+        .default
+        .addObserver(forName: NextNumberNotification.name,
+                     object: nil, queue: nil) { notification in
+          if let userInfo = notification.userInfo,
+             let number = userInfo[NextNumberNotification.numberKey] as? Int {
+            contiunation.yield(number)
+          }
+        }
+      
+    }
+  }
+  
   func receiveNumbers(with completion: @escaping (Int) -> Void) {
     NotificationCenter
       .default
