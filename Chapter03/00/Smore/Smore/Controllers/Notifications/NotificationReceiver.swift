8467c30819e2a6bab3ea0f0f3c2504ff5b0be343
Takeshi Suzuki <tockrock@gmail.com>
2023-09-19 21:09:12 +0900

    Eliminating the middle man

--- Chapter03/00/Smore/Smore/Controllers/Notifications/NotificationReceiver.swift
+++ Chapter03/00/Smore/Smore/Controllers/Notifications/NotificationReceiver.swift
@@ -4,33 +4,12 @@ class NotificationReceiver {
   let notifications = NotificationCenter.default
     .notifications(named: NextNumberNotification.name)
   
-  var entries: AsyncStream<Entry> {
-    AsyncStream(Entry.self) { contiunation in
-      Task {
-        let asyncSequence = notifications
-          .compactMap(\.userInfo)
-          .compactMap { dictionary in
-            dictionary[NextNumberNotification.numberKey] as? Int
-          }
-          .map { number in Entry(number: number) }
-        
-        for await entry in asyncSequence {
-          contiunation.yield(entry)
-        }
-      }
+  var entries: AsyncMapSequence<AsyncCompactMapSequence<AsyncCompactMapSequence<NotificationCenter.Notifications, [AnyHashable : Any]>, Int>, Entry> {
+   notifications
+    .compactMap(\.userInfo)
+    .compactMap { dictionary in
+      dictionary[NextNumberNotification.numberKey] as? Int
     }
-  }
-  
-  func receiveNumbers(with completion: @escaping (Int) -> Void) {
-    NotificationCenter
-      .default
-      .addObserver(forName: NextNumberNotification.name,
-                   object: nil,
-                   queue: nil) { notification in
-        if let userInfo = notification.userInfo,
-           let number = userInfo[NextNumberNotification.numberKey] as? Int {
-          completion(number)
-        }
-      }
+    .map { number in Entry(number: number) }
   }
 }
