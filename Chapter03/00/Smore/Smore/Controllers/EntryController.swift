b3629870c658ccb1938cf6201b543ea36d277a7c
Takeshi Suzuki <tockrock@gmail.com>
2023-09-19 20:47:20 +0900

    Revert "Modifying Sequence"
    This reverts commit 75b4d73d5b294e4c5a7dc60eafef3f9d57ad82f7.

--- Chapter03/00/Smore/Smore/Controllers/EntryController.swift
+++ Chapter03/00/Smore/Smore/Controllers/EntryController.swift
@@ -6,15 +6,6 @@ class EntryController: ObservableObject {
   private let receiver = NotificationReceiver()
   
   init() {
-    let sequence = [7, 4, nil, 9, 1, nil, 2, 6]
-      .dropFirst()
-      .compactMap { $0 }
-      .sorted()
-      .filter { $0.isMultiple(of: 2) }
-      .map { Entry(number: $0) }
-      .map(\.imageName)
-    print(sequence)
-    
     Task {
       await listenForEntries()
     }
