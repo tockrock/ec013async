86b59eeee15f179faf81d78caff44feea104a133
Takeshi Suzuki <tockrock@gmail.com>
2023-09-19 21:14:32 +0900

    Living happily ever after

--- Chapter03/00/Smore/Smore/Controllers/Notifications/NotificationReceiver.swift
+++ Chapter03/00/Smore/Smore/Controllers/Notifications/NotificationReceiver.swift
@@ -1,15 +1,11 @@
 import Foundation
 
 class NotificationReceiver {
-  let notifications = NotificationCenter.default
+  let entries = NotificationCenter.default
     .notifications(named: NextNumberNotification.name)
-  
-  var entries: AsyncMapSequence<AsyncCompactMapSequence<AsyncCompactMapSequence<NotificationCenter.Notifications, [AnyHashable : Any]>, Int>, Entry> {
-   notifications
     .compactMap(\.userInfo)
     .compactMap { dictionary in
       dictionary[NextNumberNotification.numberKey] as? Int
     }
     .map { number in Entry(number: number) }
-  }
 }
