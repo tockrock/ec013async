195ec380409f80d3d5b01c198a076b8cf23f288f
Takeshi Suzuki <tockrock@gmail.com>
2023-09-21 19:20:18 +0900

    Task that don't end

--- Chapter03/00/SmoreNmore/SmoreNmore/Controllers/EntryController.swift
+++ Chapter03/00/SmoreNmore/SmoreNmore/Controllers/EntryController.swift
@@ -7,10 +7,12 @@ class EntryController: ObservableObject {
   @Published private(set) var entryPairs: [EntryPair] = []
   
   private let plain = AutoEntryVendor(delay: 2.0)
+  private let filled = AutoEntryVendor(delay: 1.5, isFilled: true)
   
   init() {
     Task {
       await listenForEntries()
+      await listenForEntries2()
     }
   }
 }
@@ -19,6 +21,11 @@ extension EntryController {
   private func listenForEntries() async {
     for await entry in plain.entries {
       entries.append(entry)
+    }
+  }
+  private func listenForEntries2() async {
+    for await entry in filled.entries {
+      entries2.append(entry)
     }
   }
 }
