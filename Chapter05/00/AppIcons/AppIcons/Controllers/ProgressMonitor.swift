7329bff4719538da488fbf4828b815214943d563
Takeshi Suzuki <tockrock@gmail.com>
2023-09-25 20:56:00 +0900

    ProgressMonitor

--- Chapter05/00/AppIcons/AppIcons/Controllers/ProgressMonitor.swift
+++ Chapter05/00/AppIcons/AppIcons/Controllers/ProgressMonitor.swift
@@ -1,3 +1,21 @@
 class ProgressMonitor {
-  
+    let searchTerm: String
+    private(set) var total = 0
+    private(set) var downloaded = 0
+    
+    init(for searchTerm: String) {
+        self.searchTerm = searchTerm
+    }
+}
+
+extension ProgressMonitor {
+    func reset(total: Int) {
+        self.total = total
+        downloaded = 0
+    }
+    
+    func registerImageDownload() {
+        downloaded += 1
+        print("downloaded \(downloaded)/\(total)")
+    }
 }
