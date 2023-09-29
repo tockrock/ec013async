3de3b6463374575fc001cb8b1a5c76065e87bfc5
Takeshi Suzuki <tockrock@gmail.com>
2023-09-29 18:55:55 +0900

    From class to actor

--- Chapter05/00/AppIcons/AppIcons/Controllers/Searcher.swift
+++ Chapter05/00/AppIcons/AppIcons/Controllers/Searcher.swift
@@ -1,3 +1,10 @@
-class Searcher {
-  
+actor Searcher {
+    let name: String
+    let appStore: AppStore
+    
+    init(name: String,
+         appStore: AppStore) {
+        self.name = name
+        self.appStore = appStore
+    }
 }
