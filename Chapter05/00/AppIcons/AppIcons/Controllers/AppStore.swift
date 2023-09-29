ebe41f238300414a75f97a57653a8cd87a8782fa
Takeshi Suzuki <tockrock@gmail.com>
2023-09-29 19:06:14 +0900

    Initializing Searcher

--- Chapter05/00/AppIcons/AppIcons/Controllers/AppStore.swift
+++ Chapter05/00/AppIcons/AppIcons/Controllers/AppStore.swift
@@ -1,6 +1,8 @@
 import Foundation
 import Combine
 import UIKit.UIImage
+import UIKit.UIDevice
+import Distributed
 
 @MainActor
 class AppStore: ObservableObject {
@@ -8,11 +10,31 @@ class AppStore: ObservableObject {
   @Published private(set) var images = [String: UIImage]()
   @Published private(set) var isUpdating = false
   @Published private(set) var downloadedImages = 0
+  private var searcher: Searcher?
+  @Published private(set) var otherSearchers: [Searcher] = []
+  @Published private(set) var appSearchers: [String: String] = [:]
   private var downloadTask: Task<Void, Never>?
 }
 
 extension AppStore {
+  nonisolated
+  func setSeacher(with name: String) {
+    Task {
+      let searcher = Searcher(name: name,
+                              appStore: self,
+                              actorSystem: LocalTestingDistributedActorSystem())
+      await MainActor.run {
+        self.searcher = searcher
+      }
+    }
+  }
+}
+
+extension AppStore {
   func search(for rawText: String)  {
+    if let _ = self.searcher {
+      setSeacher(with: UIDevice.current.name)
+    }
     resetSearch(for: rawText)
     downloadTask = Task {
       do {
