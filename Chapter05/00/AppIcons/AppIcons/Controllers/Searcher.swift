e4d10d863b52e002676e49ffb660f10aefd4356d
Takeshi Suzuki <tockrock@gmail.com>
2023-09-29 19:07:07 +0900

    Adjust tabbing

--- Chapter05/00/AppIcons/AppIcons/Controllers/Searcher.swift
+++ Chapter05/00/AppIcons/AppIcons/Controllers/Searcher.swift
@@ -1,26 +1,26 @@
 import Distributed
 
 distributed actor Searcher {
-    typealias ActorSystem = LocalTestingDistributedActorSystem
-    
-    let name: String
-    let appStore: AppStore
-    
-    init(name: String,
-         appStore: AppStore,
-         actorSystem: ActorSystem) {
-        self.name = name
-        self.appStore = appStore
-        self.actorSystem = actorSystem
-    }
+  typealias ActorSystem = LocalTestingDistributedActorSystem
+  
+  let name: String
+  let appStore: AppStore
+  
+  init(name: String,
+       appStore: AppStore,
+       actorSystem: ActorSystem) {
+    self.name = name
+    self.appStore = appStore
+    self.actorSystem = actorSystem
+  }
 }
 
 extension Searcher {
-    func search(for searchTerm: String) {
-        // let the other searchers know abou our
-    }
-    
-    func receive(_ searchTerm: String) {
-        // update our list of current searches
-    }
+  func search(for searchTerm: String) {
+    // let the other searchers know abou our
+  }
+  
+  func receive(_ searchTerm: String) {
+    // update our list of current searches
+  }
 }
